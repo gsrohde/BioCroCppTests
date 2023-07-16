fce097af238a03cb5ed9f537a8867440da50d85e
Scott Rohde <srohde@illinois.edu>
2023-07-15 20:07:35 -0500

    Added more module_creator tests.

--- test_module_creator.cpp
+++ test_module_creator.cpp
@@ -1,12 +1,14 @@
 #include <gtest/gtest.h>
-//#include <gmock/gmock.h>
+#include <gmock/gmock.h>
 
 #include "BioCro.h"
 
 constexpr char known_module_name[] {"harmonic_oscillator"};
+constexpr char known_module_inputs[] {"position|velocity|mass|spring_constant"}; // as a regexp
+constexpr char known_module_outputs[] {"position|velocity"}; // as a regexp
 constexpr char bogus_module_name[] {"bogus"};
 
-//using testing::MatchesRegex;
+using testing::MatchesRegex;
 
 TEST(ModuleCreatorTest, KnownModule) {
     BioCro::Module_creator creator;
@@ -14,6 +16,19 @@ TEST(ModuleCreatorTest, KnownModule) {
             creator = BioCro::Standard_BioCro_library_module_factory::retrieve(known_module_name);
         });
     ASSERT_EQ(creator->get_name(), known_module_name);
+
+    auto inputs = creator->get_inputs();
+    auto outputs = creator->get_outputs();
+
+    EXPECT_EQ(inputs.size(), 4);
+    for (auto& item : inputs) {
+        EXPECT_THAT(item, MatchesRegex(known_module_inputs));
+    }
+
+    EXPECT_EQ(outputs.size(), 2);
+    for (auto& item : outputs) {
+        EXPECT_THAT(item, MatchesRegex(known_module_outputs));
+    }
 }
 
 
