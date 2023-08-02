f4b8d77c971c28185539936e2578833a7848f39f
Scott Rohde <srohde@illinois.edu>
2023-08-02 17:23:17 -0500

    Add some tests and comments.

--- test_module_object.cpp
+++ test_module_object.cpp
@@ -1,4 +1,9 @@
-//
+// Here we show that BioCro modules  may be used directly, but that we
+// have  to  be careful  when  doing  so.   In particular,  the  input
+// quantities object passed to the  module constructor must persist at
+// least  until the  module is  run in  order to  obtain the  expected
+// output.  This  means that we can't  pass the input quantities  as a
+// state_map literal, even though it may seem natural to do so.
 
 #include <gtest/gtest.h>
 
@@ -40,6 +45,8 @@ TEST_F(ModuleObjectTest, AlteredReferencedObject)
     input_quantities = {};
     ttl.run();
 
+    // We get something near zero for output rather than what one
+    // would expect.
     EXPECT_NEAR(output.at("TTc"), 0, 1e-309);
 
 }
@@ -53,7 +60,31 @@ TEST_F(ModuleObjectTest, AlterationAfterRunOK)
     EXPECT_DOUBLE_EQ(output.at("TTc"), expected_output_value);
 }
 
-TEST_F(ModuleObjectTest, LiteralInputNotOK)
+TEST_F(ModuleObjectTest, RerunningDoublesTheOutput)
+{
+    standardBML::thermal_time_linear ttl{input_quantities, &output};
+    ttl.run();
+    ttl.run();
+
+    // This, more precisely, doubles the increase; but since we
+    // started from zero, this is the same as doubling the output
+    // value.
+    EXPECT_DOUBLE_EQ(output.at("TTc"), 2 * expected_output_value);
+}
+
+TEST_F(ModuleObjectTest, RerunningAfterAlterationBarelyAltersTheOutput)
+{
+    standardBML::thermal_time_linear ttl{input_quantities, &output};
+    ttl.run();
+    input_quantities = {};
+    ttl.run();
+
+    // When we reset the input_quantities, we get something close to
+    // what we get when we only run the module once.
+    EXPECT_DOUBLE_EQ(output.at("TTc"), expected_output_value);
+}
+
+TEST_F(ModuleObjectTest, RvalueInputNotOK)
 {
     standardBML::thermal_time_linear ttl{
                                          {{"time", input_time},
@@ -63,6 +94,24 @@ TEST_F(ModuleObjectTest, LiteralInputNotOK)
                                          &output};
     ttl.run();
 
+    // We get something near zero for output rather than what one
+    // would expect.
+    EXPECT_NEAR(output.at("TTc"), 0, 1e-309);
+
+}
+
+TEST_F(ModuleObjectTest, LiteralInputNotOK)
+{
+    standardBML::thermal_time_linear ttl{
+                                         {{"time", 200},
+                                          {"sowing_time", 100},
+                                          {"temp", 25},
+                                          {"tbase", 1}},
+                                         &output};
+    ttl.run();
+
+    // We get something near zero for output rather than what one
+    // would expect.
     EXPECT_NEAR(output.at("TTc"), 0, 1e-309);
 
 }
