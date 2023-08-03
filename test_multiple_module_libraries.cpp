6fd81874ad323a8e381830ba8e03d773a9607e82
Scott Rohde <srohde@illinois.edu>
2023-08-03 11:26:39 -0500

    Refined tests of multiple modules.

--- test_multiple_module_libraries.cpp
+++ test_multiple_module_libraries.cpp
@@ -11,9 +11,12 @@
 #endif
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 #include "BioCro_Extended.h"
 #include "print_result.h"
+
+using ::testing::MatchesRegex;
 
 using Module_factory = BioCro::Standard_BioCro_library_module_factory;
 using Module_factory_2 = BioCro::Test_BioCro_library_module_factory;
@@ -24,6 +27,7 @@ class MultipleModuleLibrariesTest : public ::testing::Test {
     }
 
     void trial_simulation() {
+        bs = get_simulator();
         auto result = bs.run_simulation();
         if (VERBOSE) print_result(result);
 
@@ -39,27 +43,18 @@ class MultipleModuleLibrariesTest : public ::testing::Test {
         exit(0);
     }
 
- private:
-    BioCro::Simulator get_simulator();
-
-    BioCro::State initial_state { {"position", 0}, {"velocity", 1}, {"elapsed_time", 0}, {"TTc", 0} };
+    // These are defaults.  Individual tests can alter them.
+    BioCro::State initial_state { {"TTc", 0} };
     BioCro::Parameter_set parameters
-        { {"mass", 10}, {"spring_constant", 0.1}, {"timestep", 1},
-          {"lat", 44}, {"longitude", -121}, {"time_zone_offset", -8}, {"year", 2023},
-          {"sowing_time", 0}, {"tbase", 10} };
+        { {"timestep", 1} };
     BioCro::System_drivers drivers
         { {"time", { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 } },
           {"temp", { 5, 8, 10, 15, 20, 20, 25, 30, 32, 40} } };
-    BioCro::Module_set steady_state_modules {
-        //Module_factory::retrieve("solar_position_michalsky"),
-        Module_factory_2::retrieve("solar_position_michalsky")
-    };
-    BioCro::Module_set derivative_modules {
-        Module_factory::retrieve("harmonic_oscillator"),
-        Module_factory_2::retrieve("elapsed_time_module"),
-        Module_factory::retrieve("thermal_time_linear"),
-        Module_factory_2::retrieve("thermal_time_linear")
-    };
+    BioCro::Module_set direct_modules {};
+    BioCro::Module_set differential_modules {};
+
+private:
+    BioCro::Simulator get_simulator();
 
     // This must be declared last because its initialization depends
     // on the parameters for its construction being initialized first:
@@ -71,8 +66,8 @@ BioCro::Simulator MultipleModuleLibrariesTest::get_simulator() {
         initial_state,
         parameters,
         drivers,
-        steady_state_modules,
-        derivative_modules,
+        direct_modules,
+        differential_modules,
         "homemade_euler",
         1,
         0.0001,
@@ -82,9 +77,50 @@ BioCro::Simulator MultipleModuleLibrariesTest::get_simulator() {
 }
 
 TEST_F(MultipleModuleLibrariesTest, CorrectSimulation) {
+    parameters.insert({{"sowing_time", 0}, {"tbase", 10} });
+    std::initializer_list<BioCro::Module_creator> additional_modules {
+        Module_factory::retrieve("thermal_time_linear"),
+        Module_factory_2::retrieve("thermal_time_linear")
+    };
+    differential_modules.insert(differential_modules.end(), additional_modules);
     ASSERT_EXIT(trial_simulation(),
                 ::testing::ExitedWithCode(0),
-                ".*")
-        << "Either there was a segmentation fault"
-        " or the time variable values are incorrect in the result.";
+                ".*");
 }
+
+// Show that direct modules having outputs in common conflict, even if
+// they are from different module libraries.
+TEST_F(MultipleModuleLibrariesTest, ConflictingModules) {
+    parameters.insert({{"lat", 44}, {"longitude", -121},
+                       {"time_zone_offset", -8}, {"year", 2023} });
+    
+    direct_modules = {Module_factory::retrieve("solar_position_michalsky"),
+                      Module_factory_2::retrieve("solar_position_michalsky")};
+
+    EXPECT_THROW(trial_simulation(), std::logic_error);
+
+    // The solar_position_michalsky module is defined identically in
+    // standardBML and testBML (except for the namespace, of course).
+    // In particular, they the same set of outputs, resulting in a
+    // "quantities defined more than once" error.
+    std::string message_match_string = "Thrown by dynamical_system::"
+        "dynamical_system: the supplied inputs cannot form a valid dynamical "
+        "system.*"
+        "The following quantities were defined more than once in the inputs:.*";
+
+    // Test that the expected exception is thrown.
+    EXPECT_THROW({
+        try
+        {
+            trial_simulation();
+        }
+        catch( const std::logic_error& e )
+        {
+            // Test that the exeception has the correct message.
+            EXPECT_THAT(e.what(), MatchesRegex(message_match_string));
+            throw;
+        }
+    }, std::logic_error);
+
+}
+
