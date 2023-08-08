2be8600c072532f16ec52d3943ad0963f573c854
Scott Rohde <srohde@illinois.edu>
2023-08-08 16:36:38 -0500

    Factor out common implementation of safe simulators into an abstract base class.

--- safe_simulators.h
+++ safe_simulators.h
@@ -5,10 +5,10 @@
 
 namespace BioCro {
 
-class Idempotent_simulator
+class Safe_simulator
 {
    public:
-    Idempotent_simulator(
+    Safe_simulator(
         // parameters passed to dynamical_system constructor
         BioCro::State const& initial_values,
         BioCro::Parameter_set const& parameters,
@@ -37,49 +37,30 @@ class Idempotent_simulator
                     adaptive_max_steps);
     }
 
+    virtual BioCro::Simulation_result run_simulation() = 0;
+
+   protected:
+    BioCro::Dynamical_system sys;
+    BioCro::Solver system_solver;
+};
+    
+class Idempotent_simulator : public Safe_simulator
+{
+    using Safe_simulator::Safe_simulator; // inherit constructor
+
+   public:
     BioCro::Simulation_result run_simulation()
     {
         sys->reset();
         return system_solver->integrate(sys);
     }
-
-   private:
-    BioCro::Dynamical_system sys;
-    BioCro::Solver system_solver;
 };
 
-class Single_use_simulator
+class Single_use_simulator : public Safe_simulator
 {
+    using Safe_simulator::Safe_simulator; // inherit constructor
+
    public:
-    Single_use_simulator(
-        // parameters passed to dynamical_system constructor
-        BioCro::State const& initial_values,
-        BioCro::Parameter_set const& parameters,
-        BioCro::System_drivers const& drivers,
-        BioCro::Module_set const& direct_mcs,
-        BioCro::Module_set const& differential_mcs,
-        // parameters passed to ode_solver_factory::create
-        std::string ode_solver_name,
-        double output_step_size,
-        double adaptive_rel_error_tol,
-        double adaptive_abs_error_tol,
-        int adaptive_max_steps)
-    {
-        // Create the system
-        sys = make_dynamical_system(initial_values, parameters,
-                                 drivers, direct_mcs,
-                                 differential_mcs);
-
-        // Create the ode_solver that will be used to solve the system
-        system_solver =
-            make_ode_solver(
-                    ode_solver_name,
-                    output_step_size,
-                    adaptive_rel_error_tol,
-                    adaptive_abs_error_tol,
-                    adaptive_max_steps);
-    }
-
     BioCro::Simulation_result run_simulation()
     {
         if (has_been_run) {
@@ -90,8 +71,6 @@ class Single_use_simulator
     }
 
    private:
-    BioCro::Dynamical_system sys;
-    BioCro::Solver system_solver;
     bool has_been_run {false};
 };
 
