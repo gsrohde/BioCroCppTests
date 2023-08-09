29a25445667b271041978deb5ea14d93a12e4dff
Scott Rohde <srohde@illinois.edu>
2023-08-09 15:01:50 -0500

    Added comments and another test.

--- test_biocro.cpp
+++ test_biocro.cpp
@@ -1,3 +1,8 @@
+// Compile with the flag -DVERBOSE=true to get verbose output.
+#ifndef VERBOSE
+#define VERBOSE false
+#endif
+
 #include <gtest/gtest.h>
 
 #include "BioCro.h"
@@ -5,14 +10,26 @@
 
 using Module_factory = BioCro::Standard_BioCro_library_module_factory;
 
+/*
+ * Here we test basic usage of a Simulator object, showing how to
+ * construct and run such objects.  First we show how construction
+ * works using named arguments of the requisite types for the
+ * system-related parameters.  Then we show we can supply arguments
+ * directly as initializer lists.
+ *
+ * The form of the tests is slightly unusual: we call a function that
+ * exits with code 0 if no problems were encountered, and the tests
+ * test that the function did indeed exit with code 0.
+ */
+
 BioCro::Simulator get_simulation() {
 
-    BioCro::State initial_state { {"position", 0}, {"velocity", 1}};
+    BioCro::State initial_state { {"position", 0}, {"velocity", 1} };
     BioCro::Parameter_set parameters
-        { {"mass", 10}, {"spring_constant", 0.1}, {"timestep", 1}};
+        { {"mass", 10}, {"spring_constant", 0.1}, {"timestep", 1} };
     BioCro::System_drivers drivers
         { {"time",  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }} };
-    BioCro::Module_set steady_state_modules(0);
+    BioCro::Module_set steady_state_modules {};
     BioCro::Module_set derivative_modules {
         Module_factory::retrieve("harmonic_oscillator")
     };
@@ -39,17 +56,10 @@ class BioCroSimulationTest : public ::testing::Test {
 
     void trial_simulation() {
         auto result = bs.run_simulation();
-        //print_result(result);
+        if (VERBOSE) print_result(result);
 
-        for (auto i = 0; i < result["time"].size(); ++i) {
-            bool eq {result["time"][i] == i};
-            if (!eq) {
-                cout << "result[\"time\"][" << i << "] (" << result["time"][i]
-                     << ") != i (" << i << ")." << endl;
-                exit(1);
-            }
-        }
-
+        // If we get here, constructing the simulator and running the
+        // simulation proceeded normally.
         exit(0);
     }
 };
@@ -58,7 +68,33 @@ TEST_F(BioCroSimulationTest, CorrectSimulation) {
 
     ASSERT_EXIT(trial_simulation(),
                 ::testing::ExitedWithCode(0),
-                ".*")
-        << "Either there was a segmentation fault"
-        " or the time variable values are incorrect in the result.";
+                ".*");
+}
+
+// This test shows that we don't need to declare variables with types
+// State, Parameter_set, System_drivers, and Module_set in order to
+// construct a Simulator.  Instead, we can supply the arguments
+// directly as initializer lists.
+TEST_F(BioCroSimulationTest, SimulatorConstructedFromInitializerLists) {
+    bs = BioCro::Simulator(
+
+        // system-related arguments
+        { {"position", 0}, {"velocity", 1} },
+        { {"mass", 10}, {"spring_constant", 0.1}, {"timestep", 1} },
+        { {"time",  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }} },
+        {},
+        {Module_factory::retrieve("harmonic_oscillator")},
+
+        // solver-related arguments
+        "homemade_euler",
+        1,
+        0.0001,
+        0.0001,
+        200
+
+    );
+    
+    ASSERT_EXIT(trial_simulation(),
+                ::testing::ExitedWithCode(0),
+                ".*");
 }
